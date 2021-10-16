77fa52b416aafd44144dbdc5ff066ab8b50f97ff
Daniel Flöter <d.floe@gmx.de>
2021-10-16 12:46:24 +0200

    gallery with inner image blocks (Gutenberg 11.7)

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ function addAttributes(settings, name) {
 		settings = Object.assign(settings, {
 			icon: {
 				src: <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.2 8v11c0 .7-.6 1.2-1.2 1.2H6v1.5h13c1.5 0 2.7-1.2 2.7-2.8V8h-1.5zM18 16.4V4.6c0-.9-.7-1.6-1.6-1.6H4.6C3.7 3 3 3.7 3 4.6v11.8c0 .9.7 1.6 1.6 1.6h11.8c.9 0 1.6-.7 1.6-1.6zM4.5 4.6c0-.1.1-.1.1-.1h11.8c.1 0 .1.1.1.1V12l-2.3-1.7c-.3-.2-.6-.2-.9 0l-2.9 2.1L8 11.3c-.2-.1-.5-.1-.7 0l-2.9 1.5V4.6zm0 11.8v-1.8l3.2-1.7 2.4 1.2c.2.1.5.1.8-.1l2.8-2 2.8 2v2.5c0 .1-.1.1-.1.1H4.6c0-.1-.1-.2-.1-.2z"></path></svg>,
-				//background: '#62beda',
 				foreground: '#62beda',
 			}
 		});
@@ -35,26 +34,6 @@ addFilter(
 	'core/gallery',
 	addAttributes
 );
-
-// function addBackgroundColorStyle( props, blockType, attributes ) {
-	
-// 	if (blockType.name == 'core/gallery') {
-// 		const { color } = attributes;
-		
-// 		if (color != undefined) {
-// 			//const root = document.documentElement;
-// 			//root?.style.setProperty("--line-color", color);
-// 		}
-// 	}
-
-// 	return props;
-// }
- 
-// addFilter(
-//     'blocks.getSaveContent.extraProps',
-//     'core/gallery',
-//     addBackgroundColorStyle
-// );
 
 addFilter(
 	'editor.BlockEdit',
