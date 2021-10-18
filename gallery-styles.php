ab938f7e61ce1bcb2d4d809715fe1fa94c9dac9b
Daniel Flöter <d.floe@gmx.de>
2021-10-18 17:22:15 +0200

    Tests with Gutenberg beta

--- gallery-styles.php
+++ gallery-styles.php
@@ -35,6 +35,7 @@ add_filter( 'render_block', 'custom_block_wrapper', 10, 2 );
  * Enqueue Block Styles Javascript and Styles Stylesheet for editing
  */
 function custom_gutenberg_scripts() {
+
     wp_enqueue_style( 'editor-style-index-css',
       plugins_url( '/build/index.css', __FILE__ ),
       array(),
@@ -54,11 +55,13 @@ add_action( 'enqueue_block_editor_assets', 'custom_gutenberg_scripts' );
  * Enqueue Block Styles Stylesheet for editor and front-end
  */
 function custom_gutenberg_styles() {
-	 wp_enqueue_style( 'style-index-css',
-	 	plugins_url( '/build/style-index.css', __FILE__ ),
-     array(),
-     filemtime( plugin_dir_path( __FILE__ ) . 'build/style-index.css' )
-	);
+
+  wp_enqueue_style( 'style-index-css',
+    plugins_url( 'build/style-index.css', __FILE__ ),
+    array(),
+    filemtime( plugin_dir_path( __FILE__ ) . 'build/style-index.css' )
+  );
+  
 }
 add_action( 'enqueue_block_assets', 'custom_gutenberg_styles' );
 
